959c01dcd177adadeaf8cfb4b34f813e70bd6e8f
mohamedMallahi <kingcoding17@gmail.com>
2022-01-02 12:21:19 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,13 +2,53 @@ import React, { useState } from 'react';
 import './style.css';
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
+  const [items, setItems] = useState([]);
+  const [text, setText] = useState('');
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.length === 0) {
+      return;
+    }
+    const newItem = {
+      text:text,
+      id: Date.now()
+    };
+    setItems(prevItems => (prevItems.concat(newItem)));
+    setText('')
+  }
 
   return (
     <div>
-      <h1>Trello Clone</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <button onClick={() => setCounter((prev) => prev + 1)}>{counter}</button>
-    </div>
+    <h3>TODO</h3>
+    <TodoList items={items} />
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="new-todo">
+        What needs to be done?
+      </label>
+      <input
+        id="new-todo"
+        onChange={handleChange}
+        value={text}
+      />
+      <button>
+        Add #{items.length + 1}
+      </button>
+    </form>
+  </div>
   );
+}
+
+const TodoList = ({ items }) => {
+    return (
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    );
 }
